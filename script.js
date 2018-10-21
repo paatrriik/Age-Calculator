0fa4ede8e692450ee3edc9f7725eca68abb82d99
Ivo Rytir <irytir@gmail.com>
2018-10-20 21:20:05 -0700

    CSS cleanup

--- script.js
+++ script.js
@@ -23,23 +23,23 @@ function getBirthYear() {
     var incorrectAge = document.getElementById('incorrectAge');
     if (ageInput.value == null || ageInput.value.length == 0) {
         document.getElementById('age').innerHTML = "--";
-        correctAge.setAttribute("style", "display:block");
+        correctAge.setAttribute("style", "display:inline-block");
         incorrectAge.setAttribute("style", "display:none");
     }
     else if (ageInput.value > getCurrentYear()) {
         correctAge.setAttribute("style", "display:none");
-        incorrectAge.setAttribute("style", "display:block");
+        incorrectAge.setAttribute("style", "display:inline-block");
         document.getElementById('incorrectAgeExplanation').innerHTML = "You haven't been born yet.";
     }
     else if (ageInput.value < 1903) {
-        correctAge.setAttribute("style", "display:block");
-        incorrectAge.setAttribute("style", "display:block");
+        correctAge.setAttribute("style", "display:inline-block");
+        incorrectAge.setAttribute("style", "display:inline-block");
         document.getElementById('age').innerHTML = getCurrentYear() - ageInput.value;
         document.getElementById('incorrectAgeExplanation').innerHTML = "Congratulations, you are the oldest living person!";
     }
     else {
         document.getElementById('age').innerHTML = getCurrentYear() - ageInput.value;
-        correctAge.setAttribute("style", "display:block");
+        correctAge.setAttribute("style", "display:inline-block");
         incorrectAge.setAttribute("style", "display:none");
     }
 }
